0630be6f105ee6a87be69ec0057d664f421a1d2e
Muhammad Asad <masadasif451@gmail.com>
2024-01-26 18:18:21 -0800

    Views

---
+++ Views_in _Android/app/src/main/java/com/example/app2/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.app2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button showButton, nextButton;
+    RadioGroup radioGroup;
+    RadioButton radioButton1, radioButton2;
+    CheckBox checkBox;
+    TextView textView;
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    Switch switchButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        showButton = findViewById(R.id.showButton);
+        nextButton = findViewById(R.id.nextButton);
+        radioGroup = findViewById(R.id.radioGroup);
+        radioButton1 = findViewById(R.id.radioButton1);
+        radioButton2 = findViewById(R.id.radioButton2);
+        checkBox = findViewById(R.id.checkBox);
+        switchButton = findViewById(R.id.switchButton);
+        textView = findViewById(R.id.textView);
+
+        showButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editText.getText().toString();
+                textView.setText(text);
+            }
+        });
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NextActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
